cc03295f72916039d7335dd91c3889080a20e361
Aman Mall <amankumarmall2611@gmail.com>
2023-09-17 05:39:58 +0530

    updating

--- seeds/index.js
+++ seeds/index.js
@@ -19,11 +19,25 @@ const seedDB = async () => {
         const price = Math.floor(Math.random() * 2000) + 2000;
         const random1000 = Math.floor(Math.random() * 1000);
         const camp = new Campground({
+            author: '64fdb942b395971b1abefa78',
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
             description: 'Great place',
-            image: 'https://source.unsplash.com/collection/483251',
-            price
+            price,
+            geometry: {
+                type: 'Point', coordinates: [cities[random1000].longitude,
+                cities[random1000].latitude,]
+            },
+            images: [
+                {
+                    url: 'https://res.cloudinary.com/douqbebwk/image/upload/v1600103881/YelpCamp/lz8jjv2gyynjil7lswf4.png',
+                    filename: 'YelpCamp/cefpfjlvlp5g7quocktg',
+                },
+                {
+                    url: 'https://res.cloudinary.com/douqbebwk/image/upload/v1600103881/YelpCamp/lz8jjv2gyynjil7lswf4.png',
+                    filename: 'YelpCamp/ipi2jv42owjx6fxqwxnj',
+                }
+            ]
         })
         await camp.save();
     }
